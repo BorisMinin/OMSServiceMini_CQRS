48a4b4eff3c3455754de0b15f35d643c2e303c43
BorisMinin <32355926+BorisMinin@users.noreply.github.com>
2023-01-07 16:16:59 +0300

    Initial commit

---
+++ OMSServiceMini/Controllers/ShippersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OMSServiceMini.Data;
+
+namespace OMSServiceMini.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShippersController : ControllerBase
+    {
+        readonly NorthwindContext _northwindContext;
+
+        public ShippersController(NorthwindContext northwindContext)
+        {
+            _northwindContext = northwindContext;
+        }
+    }
+}
